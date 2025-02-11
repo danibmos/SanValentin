17d467e54adbf58266b1f206ba9a0829a8db74f4
danibmos <danielblancomosquera@gmail.com>
2025-02-11 20:27:48 +0100

    Pequeñas features

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function changePage(direction) {
     document.getElementById(`page${currentPage}`).classList.remove('active');
     currentPage += direction;
     document.getElementById(`page${currentPage}`).classList.add('active');
-    
+
     // Desactivar los botones prev y next si estamos en los extremos
     document.getElementById('prevButton').disabled = currentPage === 1;
     if (currentPage === totalPages) {
@@ -37,7 +37,7 @@ function changeQuizPage(direction) {
     document.getElementById(`quiz${currentQuizPage}`).classList.remove('active');
     currentQuizPage += direction;
     document.getElementById(`quiz${currentQuizPage}`).classList.add('active');
-    
+
     document.getElementById('prevQuiz').disabled = currentQuizPage === 1;
     if (currentQuizPage === totalQuizPages) {
         document.getElementById('nextQuiz').style.display = 'none';
@@ -63,25 +63,25 @@ function checkQuiz() {
         q11: "uxia",
         q12: "cocacola",
         q13: "gato",
-        q14: "tata",  
-        q15: "altea", 
-        q16: "sonic2",  
-        q17: "verano",  
-        q18: "aleman",     
-        q19: "capuccino",    
-        q20: "terminarañocontigo"   
+        q14: "tata",
+        q15: "altea",
+        q16: "sonic2",
+        q17: "verano",
+        q18: "aleman",
+        q19: "capuccino",
+        q20: "terminarañocontigo"
     };
-    
+
     let score = 0;
     let correctAnswers = [];
     let incorrectAnswers = [];
     let unansweredQuestions = [];
-    
+
     // Comprobar todas las preguntas
     for (let i = 1; i <= 20; i++) {
         if (i !== 3) { // Excluir la pregunta 3 de la verificación especial
             const selectedAnswer = document.querySelector(`input[name="q${i}"]:checked`);
-            
+
             if (selectedAnswer) {
                 if (selectedAnswer.value === answers[`q${i}`]) {
                     score++;
@@ -109,8 +109,27 @@ function checkQuiz() {
     }
 
     // Mostrar el resultado
-    document.getElementById('quizResult').innerText = `Has acertado ${score} de 20 preguntas.`;
-    
+    // Función para mostrar un mensaje personalizado según la puntuación del quiz
+    function showQuizMessage(score) {
+        const quizResult = document.getElementById('quizResult');
+        if (score >= 20) {
+            quizResult.innerText = `¡Felicidades mi amor! Has acertado todas las preguntas. ¡Una ración doble de besos para esta mujer!`;
+        } else if (score > 16) {
+            quizResult.innerText = `Mi amor as acertado ${score} de 20 preguntas. ¡Eso es casi perfecto! Te quierooo`;
+        }
+        else if (score > 10) {
+            quizResult.innerText = `Mi amor as acertado ${score} de 20 preguntas. ¡Pensaba que acertarias mas, pero estas aprobada!`;
+        } else if (score = 10) {
+            quizResult.innerText = `¡Ayayay! Has acertado ${score} de 20 preguntas. ¡Un 5 raspado, como en el instituto!`;
+        }
+        else {
+            quizResult.innerText = `Has acertado ${score} de 20 preguntas. ¡Me esperaba que esto si que lo aprobaras... Te quiero igual!`;
+        }
+    }
+
+    // Llamar a esta función después de calcular el score en checkQuiz()
+    showQuizMessage(score);
+
     // Mostrar las respuestas correctas
     const correctList = document.getElementById('correctAnswers');
     correctList.innerHTML = '<strong>Respuestas Correctas:</strong>';
@@ -145,7 +164,7 @@ const startDate = new Date('2024-12-01T00:00:00'); // Fecha de inicio (ejemplo: 
 function updateElapsedTime() {
     const currentDate = new Date(); // Fecha actual
     const timeDiff = currentDate - startDate; // Diferencia de tiempo en milisegundos
-    
+
     // Cálculo de años, meses, días, horas, minutos, segundos, centésimas y milisegundos
     const years = Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 365.25));
     const months = Math.floor((timeDiff % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24 * 30.44));
@@ -171,29 +190,29 @@ function updateElapsedTime() {
 setInterval(updateElapsedTime, 1);
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // Configuración de la galería
     const imagesPerPageGallery = 6; // Número de imágenes por página
     const allImages = [
-            "img/fotos/imagen1.jpg",
-            "img/fotos/imagen2.jpg",
-            "img/fotos/imagen3.jpg",
-            "img/fotos/imagen4.jpg",
-            "img/fotos/imagen5.jpg",
-            "img/fotos/imagen6.jpg",
-            "img/fotos/imagen7.jpg",
-            "img/fotos/imagen8.jpg",
-            "img/fotos/imagen9.jpg",
-            "img/fotos/imagen10.jpg",
-            "img/fotos/imagen11.gif",
-            "img/fotos/imagen12.jpg",
-            "img/fotos/imagen13.jpg",
-            "img/fotos/imagen14.jpg",
-            "img/fotos/imagen15.jpg",
-            "img/fotos/imagen16.jpg",
-            "img/fotos/imagen17.jpg",
-            "img/fotos/imagen18.jpg",
-            "img/fotos/imagen19.jpg"
+        "img/fotos/imagen1.jpg",
+        "img/fotos/imagen2.jpg",
+        "img/fotos/imagen3.jpg",
+        "img/fotos/imagen4.jpg",
+        "img/fotos/imagen5.jpg",
+        "img/fotos/imagen6.jpg",
+        "img/fotos/imagen7.jpg",
+        "img/fotos/imagen8.jpg",
+        "img/fotos/imagen9.jpg",
+        "img/fotos/imagen10.jpg",
+        "img/fotos/imagen11.gif",
+        "img/fotos/imagen12.jpg",
+        "img/fotos/imagen13.jpg",
+        "img/fotos/imagen14.jpg",
+        "img/fotos/imagen15.jpg",
+        "img/fotos/imagen16.jpg",
+        "img/fotos/imagen17.jpg",
+        "img/fotos/imagen18.jpg",
+        "img/fotos/imagen19.jpg"
     ];
 
     let currentPageGallery = 1; // Página actual
@@ -216,7 +235,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
             galleryElement.appendChild(imgElement);
         });
-        
+
         updatePagination();
     }
 
@@ -294,16 +313,16 @@ const siButton = document.getElementById('si');
 const respuesta = document.getElementById('respuesta');
 
 // Función para mover y desaparecer el botón "No"
-noButton.addEventListener("click", function() {
+noButton.addEventListener("click", function () {
     this.style.display = "none"; // Hace desaparecer el botón "No" al hacer clic
 });
 
-noButton.addEventListener("mouseover", function() {
+noButton.addEventListener("mouseover", function () {
     this.style.display = "none"; // Hace desaparecer el botón "No" al poner el ratón encima
 });
 
 // Función para mostrar la respuesta cuando se hace clic en "Sí"
-siButton.addEventListener("click", function() {
+siButton.addEventListener("click", function () {
     respuesta.innerHTML = "¡Sabía que querías!"; // Cambia el contenido del div
     respuesta.classList.add("positivo"); // Añade clase para dar estilo
     respuesta.style.display = "block"; // Muestra el div con la respuesta
@@ -345,12 +364,12 @@ function redirectToGoogle() {
 }
 
 function closePage() {
-        window.location.href = "https://www.google.com"; // Redirige a Google
-    }
+    window.location.href = "https://www.google.com"; // Redirige a Google
+}
 
 
 // Añadir atajo de teclado para Enter
-document.getElementById('password-input').addEventListener('keydown', function(event) {
+document.getElementById('password-input').addEventListener('keydown', function (event) {
     if (event.key === "Enter") {
         checkPassword(); // Llamar a la función checkPassword cuando se presiona Enter
     }
